73a747a2c0c93e433f30bc782fc0a81eedcdad64
Nathanael McDowell <nathanael.a.mcdowell@gmail.com>
2025-09-25 23:28:43 -0600

    dom theme and basics

---
+++ ponder/dom-basics.js
@@ -0,0 +1,8 @@
+const newP = document.createElement("p");
+newP.textContent = "New paragraph, whoopdeedoo";
+document.body.append(newP);
+
+const newImg = document.createElement("img");
+newImg.setAttribute("src", ); 
+newImg.setAttribute("alt", "duh image");
+document.body.append(newImg);
