7d1259963a154e45ec41152ffc0c01d3d9fa16ae
NathanMcDowell <nathanael.a.mcdowell@gmail.com>
2025-10-21 12:50:17 -0600

    got responsive functionality working

--- ponder/responsive.js
+++ ponder/responsive.js
@@ -1,1 +1,14 @@
-let 
+let button = document.querySelector(".menu-btn");
+let bar1 = document.querySelector(".bar1");
+let bar2 = document.querySelector(".bar2");
+let bar3 = document.querySelector(".bar3");
+
+let nav = document.querySelector("nav")
+
+button.addEventListener("click", changeButton);
+
+function changeButton(){
+    button.classList.toggle("change");
+    nav.classList.toggle("show")
+}
+
