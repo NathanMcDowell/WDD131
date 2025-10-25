76fe4ab69682cf443827eb023d77f15a30dd8f0c
NathanMcDowell <nathanael.a.mcdowell@gmail.com>
2025-10-24 21:23:13 -0600

    blog part 2 done

--- blog/blog.js
+++ blog/blog.js
@@ -23,5 +23,38 @@ const articles = [
 		ages: '12-16',
 		genre: 'Fantasy',
 		stars: '⭐⭐⭐⭐'
+	},
+	{
+	id: 3,
+	title: "Belgariad Book One: Pawn of Prophecy",
+	date: "Feb 12, 2022",
+	description:
+	"A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his 'Aunt Pol' and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.",
+	imgSrc:
+	"https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
+	imgAlt: "Book cover for Pawn of Prophecy",
+	ages: "12-16",
+	genre: "Fantasy",
+	stars: "⭐⭐⭐⭐⭐"
 	}
 ]
+
+function bookTemplate(book) {
+	return `<article class="book">
+	<div id="left-bar">
+        <p>${book.date}</p>
+        <p>${book.ages}</p>
+        <p>${book.genre}</p>
+        <p>${book.stars}</p>
+    </div>
+    <div id="book-bar">
+        <h2>${book.title}</h2>
+        <img src="${book.imgSrc}" alt="${book.imgAlt}">
+        <p>${book.description}
+            <span class="highlight">Read More...</span></p>
+        </div>
+		</article>`
+	}
+const htmlStrings = articles.map(bookTemplate);
+document.querySelector("#article-list").innerHTML = htmlStrings.join("")
+console.log(htmlStrings);
