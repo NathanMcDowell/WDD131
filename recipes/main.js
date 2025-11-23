f6401090657b0d6eaa49a083dfe10c76d75155d9
NathanMcDowell <nathanael.a.mcdowell@gmail.com>
2025-11-22 17:29:43 -0700

    Rewrote the recipe template using info from the assignment.

--- recipes/main.js
+++ recipes/main.js
@@ -3,21 +3,30 @@ import recipes from "./recipes.mjs";
 function recipeTemplate(recipe) {
     console.log("recipeTemplate has started")
     console.log(recipes.length)
-    return `<img src="${recipe.image}" alt="${recipe.name}">
-            <div>
-            <div class="tag">
-                <p>Dessert</p>
-            </div>
-                <h2>${recipe.name}</h2>
-                <span class="rating" role="img" aria-label="Rating: 4 out of 5 stars">
-                    <span aria-hidden="true" class="icon-star">⭐</span>
-                    <span aria-hidden="true" class="icon-star">⭐</span>
-                    <span aria-hidden="true" class="icon-star">⭐</span>
-                    <span aria-hidden="true" class="icon-star-empty">⭐</span>
-                    <span aria-hidden="true" class="icon-star-empty">☆</span>
-                </span>
-                <p class="recipe-description">${recipe.description}</p>
-            </div>`
+    return `<figure class="recipe">
+	<img src="${recipe.image}" alt="${recipe.name}" />
+	<figcaption>
+		<ul class="recipe__tags">
+			<li>Dessert</li>
+			<li>Fruit</li>
+		</ul>
+		<h2><a href="#">${recipe.name}</a></h2>
+		<p class="recipe__ratings">
+			<span
+				class="rating"
+				role="img"
+				aria-label="Rating: 3 out of 5 stars"
+			>
+				<span aria-hidden="true" class="icon-star">⭐</span>
+				<span aria-hidden="true" class="icon-star">⭐</span>
+				<span aria-hidden="true" class="icon-star">⭐</span>
+				<span aria-hidden="true" class="icon-star">⭐</span>
+				<span aria-hidden="true" class="icon-star-empty">☆</span>
+			</span>
+		</p>
+		<p class="recipe-description">${recipe.description}</p>
+</figcaption>
+</figure>`;
 }
 const randomNum = Math.floor(Math.random()*(recipes.length));
 const testRecipeContent = recipeTemplate(recipes[randomNum]);
