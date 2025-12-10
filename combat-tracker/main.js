07747361071e8638ecdf023bd038b74b52067ab4
NathanMcDowell <nathanael.a.mcdowell@gmail.com>
2025-12-10 11:11:09 -0700

    The list writes out properly now.

---
+++ combat-tracker/main.js
@@ -0,0 +1,64 @@
+const character = {
+    name: "John the Barbarian",
+    hp: 20,
+    roll: 13,
+    resistances: []
+};
+
+
+
+const trackerBox = document.querySelector("#initiative-list")
+
+trackerBox.innerHTML = localStorage.getItem('John the Barbarian')
+
+function initiativeHtmlMaker()
+{
+    let html = 
+    `<thead>
+        <tr>
+            <th></th>
+            <th></th>
+            <th>Name</th>
+            <th>HP</th>
+            <th>Resistances</th>
+        </tr>
+    </thead>
+    <tbody>`
+    const allItems = Object.entries(localStorage);
+    console.log("allItems:");
+    console.log(allItems);
+    allItems.slice().reverse().forEach(([key, value]) => {
+        const character = JSON.parse(value)
+        console.log(character)
+        html += `<tr>
+        <td><button class="damage-button">Damage</button></td>
+        <td><button class="heal-button">Heal</button></td>
+        <td>${character.name}<td>
+        <td>${character.hp}</td>
+        <td>${character.resistances}</td>
+        <td>${character.roll}</td>
+        </tr>`
+    })
+    html += `</tbody>`;
+    return html;
+}
+
+function submitHandler()
+{
+    const name = document.querySelector("#char-name").value;
+    const hp = document.querySelector("#char-hp").value;
+    const roll = document.querySelector("#dex-roll").value;
+    const resistances = document.querySelector("#resistances").value;
+    const character = {name, hp, roll, resistances};
+
+    console.log(character);
+    if(name != ""){
+        localStorage.setItem(name, JSON.stringify(character));
+    }
+    
+    
+    trackerBox.innerHTML = initiativeHtmlMaker()
+}
+
+
+document.querySelector("#submit-button").addEventListener("click", submitHandler)
